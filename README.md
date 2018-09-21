482765936c797f83ea4b27ec7e7cea2062245108
Alastair7 <lekhidugtal@gmail.com>
2018-09-21 22:47:53 +0530

    prototype of func

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# Text-Editor
+## Text Editor
+
+```
+A tkinter based Text Editor in Python
+```
+Done in Early learning stages of python and tkinter
+
+### Features
+* File Mode : SAVE, OPEN, NEW, EXIT
+* Edit Mode : CUT, COPY, PASTE
+* Help Mode : HELLPPP
+
+### Tkinter ??
+A GUI(Graphical User Interface) Package of python.
+
+### How to Run
+```
+python3 editor.py
+```
+> Tkinter was renamed to tkinter in python3
--- editor.py
+++ editor.py
@@ -1,18 +1,41 @@
-from tkinter import*
-import tkinter.scrolledtext as ScrolledText
+from tkinter import Tk, scrolledtext, Menu, filedialog
+
 
 root = Tk(className= " Untitled-Editor")
-textArea = ScrolledText.ScrolledText(root, width=300, height=200)
+textArea = scrolledtext.ScrolledText(root, width=200, height=150)
 
 menubar = Menu(root)
 root.config(menu=menubar)
 filemenu = Menu(menubar)
-menubar.add_cascade(label = "File", menu=filemenu)
-filemenu.add_command(label = "New")
-filemenu.add_command(label = "Open")
-filemenu.add_command(label = "Save")
-filemenu.add_command(label = "Exit")
+editmenu = Menu(menubar)
+helpmenu = Menu(menubar)
 
+#adding options to our menu
+menubar.add_cascade(label = "File", menu=filemenu)
+menubar.add_cascade(label = "Edit", menu=editmenu)
+menubar.add_cascade(label= "Help", menu=helpmenu)
+
+#adding to filemenu
+def newFile():
+
+# def openFile():
+# def saveFile():
+# def exitFile():
+filemenu.add_command(label = "New",command = newFile)
+filemenu.add_command(label = "Open",command = openFile)
+filemenu.add_command(label = "Save",command = saveFile)
+filemenu.add_separator()
+filemenu.add_command(label = "Exit",command = exitFile)
+
+#adding to our editmenu
+editmenu.add_cascade(label="Cut")
+editmenu.add_cascade(label="Copy")
+editmenu.add_cascade(label="Paste")
+
+#adding to our help menu
+helpmenu.add_cascade(label="Help")
+helpmenu.add_separator()
+helpmenu.add_cascade(label="About")
 
 
 textArea.pack()
